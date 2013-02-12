7bd7c4b3e208d96f7da3ea302c85b9836433d94b
DTrejo <david.daniel.trejo@gmail.com>
2013-02-12 15:56:35 -0500

    rm stylus.js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 var http = require('http');
 var path = require('path')
+
 var ErrorPage = require('error-page')
-
 var ecstatic = require('ecstatic');
+
 var PUBLIC = path.join(__dirname + '/' + 'public')
-var static = ecstatic({ root: PUBLIC, autoIndex: true })
-
-var stylus = require('./stylus')
-
 var PORT = 8080
 // var IS_PROD = process.env.NODE_ENV === 'production'
+
+var static = ecstatic({ root: PUBLIC, autoIndex: true })
+var stylus = require('stylus').middleware({ src: PUBLIC })
+
+
 
 http.createServer(function(req, res) {
   res.error = ErrorPage(req, res, { debug: true })
 
   return stylus(req, res, function(er) {
     if (er) return res.error(er)
+
     return static(req, res)
   })
-}).listen(8080)
+}).listen(PORT)
 
 console.log('serving dtrejo.com on http://localhost:' + PORT
   + ' with ecstatic@' + ecstatic.version + ' & node@' + process.version
