b23ce00433c9c1fa2adc581b001013c628d11eaa
DTrejo <david.daniel.trejo@gmail.com>
2013-02-25 18:25:55 -0500

    browserifyWatcher: support multiple bundles

--- browserifyWatcher.js
+++ browserifyWatcher.js
@@ -8,26 +8,36 @@ var browserify = require('browserify')
 // Takes a path to a js file, outputs browserified version, e.g.
 // dir/filename.js -> dir/filename.min.js
 //
-function browserifyWatcher (mainjs) {
-  var b = browserify({ watch: true, debug: true })
-  b.addEntry(mainjs)
+function browserifyWatcher (jsfiles) {
+  if (typeof jsfiles == 'string') jsfiles = [ jsfiles ]
 
-  var newname = path.basename(mainjs, '.js') + '.min.js'
-  var output = path.join(path.dirname(mainjs), newname)
+  var bundles = []
+  jsfiles.forEach(function(mainjs) {
+    var b = browserify({ watch: true, debug: true })
+    bundles.push(b)
+    b.addEntry(mainjs)
 
-  // force-regen on startup, like when deployed
-  fs.exists(output, function(exists) {
-    if (exists) return
-    onBundle()
+    var newname = path.basename(mainjs, '.js') + '.min.js'
+    var output = path.join(path.dirname(mainjs), newname)
+    var prettypath = path.relative(path.dirname(module.parent.filename), output)
+
+    // force-regen on startup, like when deployed
+    fs.exists(output, function(exists) {
+      if (exists) return
+      onBundle()
+    })
+
+    b.on('bundle', onBundle);
+    function onBundle() {
+      fs.writeFile(output, b.bundle(), 'utf8', onWrite);
+    }
+    function onWrite () {
+      console.log('Updated bundle -', b.modified.toISOString()
+        , '-', prettypath)
+    }
+    b.on('syntaxError', function(err) {
+      console.log(err.toString())
+    })
   })
-
-  b.on('bundle', onBundle);
-  function onBundle() {
-    fs.writeFile(output, b.bundle(), 'utf8', onWrite);
-  }
-  function onWrite () {
-    console.log('Updated bundle -', b.modified.toISOString()
-      , '-', path.relative(path.dirname(module.parent.filename), output))
-  }
-  return b;
+  return bundles;
 }
